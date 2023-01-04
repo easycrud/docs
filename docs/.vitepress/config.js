c7fd99f844124eb4d56a304badee2da375e40a81
xilesun <2013xile@gmail.com>
2023-01-04 11:48:20 +0800

    add logo

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,5 +2,11 @@ import { defineConfig } from 'vitepress'
 
 export default defineConfig({
   title: 'EASYCRUD',
-  description: 'Faster the Development of CRUD Web Applications.'
+  description: 'Faster the Development of CRUD Web Applications.',
+  head: [
+    ['link', { rel: "shortcut icon", href: "/logo.png"}],
+  ],
+  themeConfig: {
+    logo: '/logo.png',
+  }
 })
